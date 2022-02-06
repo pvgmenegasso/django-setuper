20e70fd1935fd092a0682b543591e7229ef8691a
Pedro Vinícius Menegasso <pvgmenegasso@MacBook-Air-de-Pedro.local>
2022-02-06 18:56:03 -0300

    Add start.sh file

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+# First we install django
+echo $(pipenv install django)
+# Check if django is installed, print in screen
+echo '/n'
+pipenv run python -m django --version
+
+# Now we create the django project based
+# on the name provided by the user
+pipenv run django-admin startproject "$1"
+
